3afbcc8239ad3c60e91ca64e401850fa6ff96000
Joshua Grosser <joshua.grosser@gmail.com>
2019-06-05 08:42:47 -0400

    - Customer search is now optimized to search on first_name, last_name, and email (exact match) with additional secondary indexes. - Customer search now loads 100 customers by default on the search page if no search term is entered.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 class CustomersController < ApplicationController
   def index
+    @order_by_clause = 'email, last_name, first_name'
     if params[:keywords].present?
       @keywords = params[:keywords]
       customer_search_term = CustomerSearchTerm.new(@keywords)
-      @customers = Customer.where(customer_search_term.where_clause)
+      @customers = Customer
+                   .where(customer_search_term.where_clause)
+                   .order(customer_search_term.order_by_clause)
     else
-      @customers = []
+      @customers = Customer.all.order(@order_by_clause).limit(100)
     end
   end
 end
--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -1,10 +1,12 @@
 # Supports searching for customers.
 class CustomerSearchTerm
   attr_reader :where_clause
+  attr_reader :order_by_clause
 
   def initialize(search_term)
     search_term = search_term.downcase
     @where_clause = ''
+    @order_by_clause = 'email, last_name, first_name'
 
     if search_term.include? '@'
       email_search search_term # search for email
@@ -14,11 +16,11 @@ class CustomerSearchTerm
   end
 
   def email_search(search_term)
-    @where_clause = "LOWER(email) LIKE '%#{search_term}%'"
+    @where_clause = "LOWER(email) = '#{search_term}'"
   end
 
   def search(search_term)
-    @where_clause = "LOWER(first_name) LIKE '%#{search_term}%'" \
-      " OR LOWER(last_name) LIKE '%#{search_term}%'"
+    @where_clause = "LOWER(first_name) LIKE '#{search_term}%'" \
+      " OR LOWER(last_name) LIKE '#{search_term}%'"
   end
 end

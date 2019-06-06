7a536d56278dd51dd27f3339cf911b79851a8403
Joshua Grosser <joshua.grosser@gmail.com>
2019-06-06 09:46:11 -0400

    - Added Angular components.

---
+++ app/javascript/packs/customers.js
@@ -0,0 +1,5 @@
+import "hello_angular/polyfills";
+import { Component, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";

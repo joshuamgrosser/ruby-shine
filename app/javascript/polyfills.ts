f1f319b52fbfbfe7b79793016a878b15bfb1d45e
Joshua Grosser <joshua.grosser@gmail.com>
2019-06-14 08:39:31 -0400

    After much wrestling, Angular is finally wired up and working correctly with Rails and Webpack.

--- app/javascript/polyfills.ts
+++ app/javascript/polyfills.ts
@@ -18,45 +18,43 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
+/* IE9, IE10 and IE11 requires all of the following polyfills. */
+import 'core-js/es6/array';
+import 'core-js/es6/date';
+import 'core-js/es6/function';
+import 'core-js/es6/map';
+import 'core-js/es6/math';
+import 'core-js/es6/number';
+import 'core-js/es6/object';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/regexp';
+import 'core-js/es6/set';
+import 'core-js/es6/string';
+import 'core-js/es6/symbol';
+import 'core-js/es6/weak-map';
+
+// required for array.includes
+import 'core-js/es7/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/** Evergreen browsers require these. **/
+/* Evergreen browsers require these. */
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
-
-/**
+/*
  * Required to support Web Animations `@angular/animation`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
+*/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';
 // import 'zone.js/dist/long-stack-trace-zone' // async stack traces with zone.js
-
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
@@ -71,3 +69,27 @@ import 'zone.js/dist/zone';
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
+
+if (!Element.prototype.matches) {
+    Element.prototype.matches = (Element.prototype as any).msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+// Element.prototype.remove polyfill
+// https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
+((arr) => {
+    arr.forEach((item) => {
+        if (item.hasOwnProperty('remove')) {
+            return;
+        }
+        Object.defineProperty(item, 'remove', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function remove() {
+                if (this.parentNode !== null) {
+                    this.parentNode.removeChild(this);
+                }
+            }
+        });
+    });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);

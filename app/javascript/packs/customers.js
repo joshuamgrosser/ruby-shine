417a387840ea9d3b3a7e52c3a9a037ca214f75fa
Joshua Grosser <joshua.grosser@gmail.com>
2019-06-14 11:53:10 -0400

    Loading static list of customers now works. Search button now fires correctly.

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -1,1 +1,39 @@
 require('../customers');
+
+var RESULTS = [
+    {
+        first_name: "Pat",
+        last_name: "Smith",
+        username: "psmith",
+        email: "pat.smith@somewhere.net",
+        created_at: "2016-02-05",
+    },
+    {
+        first_name: "Patrick",
+        last_name: "Jones",
+        username: "pjpj",
+        email: "jones.p@business.net",
+        created_at: "2014-03-05",
+    },
+    {
+        first_name: "Patricia",
+        last_name: "Benjamin",
+        username: "pattyb",
+        email: "benjie@aol.info",
+        created_at: "2016-01-02",
+    },
+    {
+        first_name: "Patty",
+        last_name: "Patrickson",
+        username: "ppat",
+        email: "pppp@freemail.computer",
+        created_at: "2016-02-05",
+    },
+    {
+        first_name: "Jane",
+        last_name: "Patrick",
+        username: "janesays",
+        email: "janep@company.net",
+        created_at: "2013-01-05",
+    },
+];
